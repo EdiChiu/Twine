976d8c09c797cf29bc88246cf3bde1928b8cbad6
EdiChiu <edisonnchiu@gmail.com>
2024-10-28 17:29:28 -0500

    Initial Commit

---
+++ Twine/TwineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwineApp.swift
+//  Twine
+//
+//  Created by Edison Chiu on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TwineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
